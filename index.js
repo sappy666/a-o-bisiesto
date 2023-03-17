2945093b3fda9e28542b1548e5f6fb5bacad9391
Maca <116607202+sappy666@users.noreply.github.com>
2023-03-17 16:19:29 -0300

    correcciones

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // Ingresar un número que represente el año
-let year = prompt("Ingresa un número que represente un año:");
+// Ingresar un número
+let anno;
+const btnEnviar = document.getElementById("btn-enviar");  
+const inputNumber = document.getElementById("nombre");
+btnEnviar.addEventListener('click', () => {
+  anno = inputNumber.value;
+  bisiesto(anno);
+});
 
 // Verificar que el número sea mayor o igual a cero
-if (year >= 0) {
+function bisiesto(year){
+  if (year >= 0) {
   // Verificar si el año es bisiesto
   if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-    console.log("El año ingresado es bisiesto");
+    alert("El año ingresado es bisiesto");
   } else {
-    console.log("El año ingresado no es bisiesto");
     alert("El año ingresado no es bisiesto");
   }
 } else {
-  console.log("El número ingresado no es válido");
   alert("El número ingresado no es válido");
+}
 }
